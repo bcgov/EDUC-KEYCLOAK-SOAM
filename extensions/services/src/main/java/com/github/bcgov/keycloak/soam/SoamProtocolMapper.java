9635aa09217becec2d93a91cd62fdf5e84136aa1
Marco Villeneuve <marco.villeneuve@gov.bc.ca>
2019-11-14 14:58:38 -0800

    Trying group mapper class direct.

--- extensions/services/src/main/java/com/github/bcgov/keycloak/soam/SoamProtocolMapper.java
+++ extensions/services/src/main/java/com/github/bcgov/keycloak/soam/SoamProtocolMapper.java
@@ -1,96 +1,137 @@
+/*
+ * Copyright 2016 Red Hat, Inc. and/or its affiliates
+ * and other contributors as indicated by the @author tags.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.github.bcgov.keycloak.soam;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-import org.jboss.logging.Logger;
-import org.keycloak.models.AuthenticatedClientSessionModel;
-import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.GroupModel;
 import org.keycloak.models.ProtocolMapperModel;
 import org.keycloak.models.UserSessionModel;
+import org.keycloak.models.utils.ModelToRepresentation;
 import org.keycloak.protocol.oidc.OIDCLoginProtocol;
 import org.keycloak.protocol.oidc.mappers.AbstractOIDCProtocolMapper;
+import org.keycloak.protocol.oidc.mappers.GroupMembershipMapper;
 import org.keycloak.protocol.oidc.mappers.OIDCAccessTokenMapper;
+import org.keycloak.protocol.oidc.mappers.OIDCAttributeMapperHelper;
 import org.keycloak.protocol.oidc.mappers.OIDCIDTokenMapper;
 import org.keycloak.protocol.oidc.mappers.UserInfoTokenMapper;
 import org.keycloak.provider.ProviderConfigProperty;
-import org.keycloak.representations.AccessToken;
 import org.keycloak.representations.IDToken;
 
-public class SoamProtocolMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper, OIDCIDTokenMapper, UserInfoTokenMapper{
+/**
+ * Maps user group membership
+ *
+ * @author <a href="mailto:bill@burkecentral.com">Bill Burke</a>
+ * @version $Revision: 1 $
+ */
+public class SoamProtocolMapper extends AbstractOIDCProtocolMapper implements OIDCAccessTokenMapper, OIDCIDTokenMapper, UserInfoTokenMapper {
 
-	public static final String PROVIDER_ID = "oidc-customprotocolmapper";
-	private static Logger logger = Logger.getLogger(SoamProtocolMapper.class);
+    private static final List<ProviderConfigProperty> configProperties = new ArrayList<ProviderConfigProperty>();
 
-	private static final List<ProviderConfigProperty> configProperties = new ArrayList<ProviderConfigProperty>();
+    static {
+        OIDCAttributeMapperHelper.addTokenClaimNameConfig(configProperties);
+        ProviderConfigProperty property1 = new ProviderConfigProperty();
+        property1.setName("full.path");
+        property1.setLabel("Full group path");
+        property1.setType(ProviderConfigProperty.BOOLEAN_TYPE);
+        property1.setDefaultValue("true");
+        property1.setHelpText("Include full path to group i.e. /top/level1/level2, false will just specify the group name");
+        configProperties.add(property1);
 
-	@Override
-	public List<ProviderConfigProperty> getConfigProperties() {
-		logger.info("SOAM: inside getConfigProperties");
-		return configProperties;
-	}
+        OIDCAttributeMapperHelper.addIncludeInTokensConfig(configProperties, GroupMembershipMapper.class);
+    }
 
-	@Override
-	public String getDisplayCategory() {
-		logger.info("SOAM: inside getDisplayCategory");
-		return TOKEN_MAPPER_CATEGORY;
-	}
+    public static final String PROVIDER_ID = "oidc-soam-group-membership-mapper";
 
-	@Override
-	public String getDisplayType() {
-		logger.info("SOAM: inside getDisplayType");
-		return "Stackoverflow Custom Protocol Mapper";
-	}
 
-	@Override
-	public String getId() {
-		logger.info("SOAM: inside getId");
-		return PROVIDER_ID;
-	}
+    public List<ProviderConfigProperty> getConfigProperties() {
+        return configProperties;
+    }
 
-	@Override
-	public String getHelpText() {
-		logger.info("SOAM: inside getHelpText");
-		return "some help text";
-	}
+    @Override
+    public String getId() {
+        return PROVIDER_ID;
+    }
 
-	@Override
-	public AccessToken transformAccessToken(AccessToken token, ProtocolMapperModel mappingModel, KeycloakSession session,
-			UserSessionModel userSession, AuthenticatedClientSessionModel clientSession) {
-		logger.info("SOAM: inside transformAccessToken");
-		token.getOtherClaims().put("stackoverflowCustomToken", "stackoverflow");
+    @Override
+    public String getDisplayType() {
+        return "Soam Protocol Map";
+    }
 
-		setClaim(token, mappingModel, userSession, session);
-		return token;
-	}
+    @Override
+    public String getDisplayCategory() {
+        return TOKEN_MAPPER_CATEGORY;
+    }
 
-	public static ProtocolMapperModel create(String name, boolean accessToken, boolean idToken, boolean userInfo) {
-		logger.info("SOAM: inside create");
-		ProtocolMapperModel mapper = new ProtocolMapperModel();
-		mapper.setName(name);
-		mapper.setProtocolMapper(PROVIDER_ID);
-		mapper.setProtocol(OIDCLoginProtocol.LOGIN_PROTOCOL);
-		Map<String, String> config = new HashMap<String, String>();
-		mapper.setConfig(config);
-		return mapper;
-	}
+    @Override
+    public String getHelpText() {
+        return "Map soam claims";
+    }
 
-	@Override
-	protected void setClaim(IDToken token, ProtocolMapperModel mappingModel, UserSessionModel userSession) {
-		logger.info("SOAM: inside setClaim");
-		super.setClaim(token, mappingModel, userSession);
-	}
+    public static boolean useFullPath(ProtocolMapperModel mappingModel) {
+        return "true".equals(mappingModel.getConfig().get("full.path"));
+    }
 
-	@Override
-	protected void setClaim(IDToken token, ProtocolMapperModel mappingModel, UserSessionModel userSession,
-			KeycloakSession keycloakSession) {
-		logger.info("SOAM: inside setClaim2");
-		super.setClaim(token, mappingModel, userSession, keycloakSession);
-	}
-	
-	
-	
-	
+
+    /**
+     * Adds the group membership information to the {@link IDToken#otherClaims}.
+     * @param token
+     * @param mappingModel
+     * @param userSession
+     */
+    protected void setClaim(IDToken token, ProtocolMapperModel mappingModel, UserSessionModel userSession) {
+
+        List<String> membership = new LinkedList<>();
+        boolean fullPath = useFullPath(mappingModel);
+        for (GroupModel group : userSession.getUser().getGroups()) {
+            if (fullPath) {
+                membership.add(ModelToRepresentation.buildGroupPath(group));
+            } else {
+                membership.add(group.getName());
+            }
+        }
+        String protocolClaim = mappingModel.getConfig().get(OIDCAttributeMapperHelper.TOKEN_CLAIM_NAME);
+
+        token.getOtherClaims().put(protocolClaim, membership);
+    }
+
+    public static ProtocolMapperModel create(String name,
+                                      String tokenClaimName,
+                                      boolean consentRequired, String consentText,
+                                      boolean accessToken, boolean idToken) {
+        ProtocolMapperModel mapper = new ProtocolMapperModel();
+        mapper.setName(name);
+        mapper.setProtocolMapper(PROVIDER_ID);
+        mapper.setProtocol(OIDCLoginProtocol.LOGIN_PROTOCOL);
+        mapper.setConsentRequired(consentRequired);
+        mapper.setConsentText(consentText);
+        Map<String, String> config = new HashMap<String, String>();
+        config.put(OIDCAttributeMapperHelper.TOKEN_CLAIM_NAME, tokenClaimName);
+        if (accessToken) config.put(OIDCAttributeMapperHelper.INCLUDE_IN_ACCESS_TOKEN, "true");
+        if (idToken) config.put(OIDCAttributeMapperHelper.INCLUDE_IN_ID_TOKEN, "true");
+        mapper.setConfig(config);
+        
+        return mapper;
+    }
+
+
 }
