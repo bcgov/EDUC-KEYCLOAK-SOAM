f62dfb2f2df919eea3723edfb6b616de4cba55be
Marco Villeneuve <marco.villeneuve@gov.bc.ca>
2019-11-15 10:42:52 -0800

    Added logging.

--- extensions/services/src/main/java/com/github/bcgov/keycloak/soam/PropertyFileUserStorageProvider.java
+++ extensions/services/src/main/java/com/github/bcgov/keycloak/soam/PropertyFileUserStorageProvider.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.jboss.logging.Logger;
 import org.keycloak.component.ComponentModel;
 import org.keycloak.credential.CredentialInput;
 import org.keycloak.credential.CredentialInputUpdater;
@@ -34,7 +35,8 @@ public class PropertyFileUserStorageProvider implements UserStorageProvider, Use
 	// map of loaded users in this transaction
 	protected Map<String, UserModel> loadedUsers = new HashMap<>();
     private static final Set<String> disableableTypes = new HashSet<>();
-
+    private static Logger logger = Logger.getLogger(PropertyFileUserStorageProvider.class);
+    
     static {
         disableableTypes.add(CredentialModel.PASSWORD);
     }
@@ -48,6 +50,7 @@ public class PropertyFileUserStorageProvider implements UserStorageProvider, Use
 
 	@Override
 	public UserModel getUserByUsername(String username, RealmModel realm) {
+		logger.info("SOAM User Storage: inside getUserByUsername");
 		UserModel adapter = loadedUsers.get(username);
 		if (adapter == null) {
 			String password = properties.get(username);
@@ -60,6 +63,7 @@ public class PropertyFileUserStorageProvider implements UserStorageProvider, Use
 	}
 
     protected UserModel createAdapter(RealmModel realm, String username) {
+    	logger.info("SOAM User Storage: inside createAdapter");
         UserModel local = session.userLocalStorage().getUserByUsername(username, realm);
         if (local == null) {
             local = session.userLocalStorage().addUser(realm, username);
@@ -79,6 +83,7 @@ public class PropertyFileUserStorageProvider implements UserStorageProvider, Use
     }
 	@Override
 	public UserModel getUserById(String id, RealmModel realm) {
+		logger.info("SOAM User Storage: inside getUserById");
 		StorageId storageId = new StorageId(id);
 		String username = storageId.getExternalId();
 		return getUserByUsername(username, realm);
@@ -86,22 +91,26 @@ public class PropertyFileUserStorageProvider implements UserStorageProvider, Use
 
 	@Override
 	public UserModel getUserByEmail(String email, RealmModel realm) {
+		logger.info("SOAM User Storage: inside getUserByEmail");
 		return null;
 	}
 
 	@Override
 	public boolean isConfiguredFor(RealmModel realm, UserModel user, String credentialType) {
+		logger.info("SOAM User Storage: inside isConfiguredFor");
 		String password = properties.get(user.getUsername());
 		return credentialType.equals(CredentialModel.PASSWORD) && password != null;
 	}
 
 	@Override
 	public boolean supportsCredentialType(String credentialType) {
+		logger.info("SOAM User Storage: inside supportsCredentialType");
 		return credentialType.equals(CredentialModel.PASSWORD);
 	}
 
     @Override
     public boolean isValid(RealmModel realm, UserModel user, CredentialInput input) {
+    	logger.info("SOAM User Storage: inside isValid");
         if (!supportsCredentialType(input.getType()) || !(input instanceof UserCredentialModel)) return false;
 
         UserCredentialModel cred = (UserCredentialModel)input;
@@ -112,6 +121,7 @@ public class PropertyFileUserStorageProvider implements UserStorageProvider, Use
 
     @Override
     public boolean updateCredential(RealmModel realm, UserModel user, CredentialInput input) {
+    	logger.info("SOAM User Storage: inside updateCredential");
         if (!(input instanceof UserCredentialModel)) return false;
         if (!input.getType().equals(CredentialModel.PASSWORD)) return false;
         UserCredentialModel cred = (UserCredentialModel)input;
@@ -124,6 +134,7 @@ public class PropertyFileUserStorageProvider implements UserStorageProvider, Use
 
     @Override
     public void disableCredentialType(RealmModel realm, UserModel user, String credentialType) {
+    	logger.info("SOAM User Storage: inside disableCredentialType");
         if (!credentialType.equals(CredentialModel.PASSWORD)) return;
         synchronized (properties) {
             properties.put(user.getUsername(), UNSET_PASSWORD);
@@ -134,7 +145,7 @@ public class PropertyFileUserStorageProvider implements UserStorageProvider, Use
 
     @Override
     public Set<String> getDisableableCredentialTypes(RealmModel realm, UserModel user) {
-
+    	logger.info("SOAM User Storage: inside getDisableableCredentialTypes");
         return disableableTypes;
     }
 
@@ -146,6 +157,7 @@ public class PropertyFileUserStorageProvider implements UserStorageProvider, Use
 
 	@Override
 	public UserModel addUser(RealmModel realm, String username) {
+		logger.info("SOAM User Storage: inside addUser");
 		synchronized (properties) {
 			properties.put(username, UNSET_PASSWORD);
 			save();
@@ -155,6 +167,7 @@ public class PropertyFileUserStorageProvider implements UserStorageProvider, Use
 
 	@Override
 	public boolean removeUser(RealmModel realm, UserModel user) {
+		logger.info("SOAM User Storage: inside removeUser");
 		synchronized (properties) {
 			if (properties.remove(user.getUsername()) == null)
 				return false;
@@ -178,16 +191,19 @@ public class PropertyFileUserStorageProvider implements UserStorageProvider, Use
 	
     @Override
     public int getUsersCount(RealmModel realm) {
+    	logger.info("SOAM User Storage: inside getUsersCount");
         return properties.size();
     }
 
     @Override
     public List<UserModel> getUsers(RealmModel realm) {
+    	logger.info("SOAM User Storage: inside getUsers");
         return getUsers(realm, 0, Integer.MAX_VALUE);
     }
 
     @Override
     public List<UserModel> getUsers(RealmModel realm, int firstResult, int maxResults) {
+    	logger.info("SOAM User Storage: inside getUsers2");
         List<UserModel> users = new LinkedList<>();
         int i = 0;
         for (Object obj : properties.keySet()) {
@@ -202,11 +218,13 @@ public class PropertyFileUserStorageProvider implements UserStorageProvider, Use
     
     @Override
     public List<UserModel> searchForUser(String search, RealmModel realm) {
+    	logger.info("SOAM User Storage: inside searchForUser4");
         return searchForUser(search, realm, 0, Integer.MAX_VALUE);
     }
 
     @Override
     public List<UserModel> searchForUser(String search, RealmModel realm, int firstResult, int maxResults) {
+    	logger.info("SOAM User Storage: inside searchForUser");
         List<UserModel> users = new LinkedList<>();
         int i = 0;
         for (Object obj : properties.keySet()) {
@@ -222,11 +240,13 @@ public class PropertyFileUserStorageProvider implements UserStorageProvider, Use
 
     @Override
     public List<UserModel> searchForUser(Map<String, String> params, RealmModel realm) {
+    	logger.info("SOAM User Storage: inside searchForUser2");
         return searchForUser(params, realm, 0, Integer.MAX_VALUE);
     }
 
     @Override
     public List<UserModel> searchForUser(Map<String, String> params, RealmModel realm, int firstResult, int maxResults) {
+    	logger.info("SOAM User Storage: inside searchForUser3");
         // only support searching by username
         String usernameSearchString = params.get("username");
         if (usernameSearchString == null) return Collections.EMPTY_LIST;
@@ -235,16 +255,19 @@ public class PropertyFileUserStorageProvider implements UserStorageProvider, Use
     
     @Override
     public List<UserModel> getGroupMembers(RealmModel realm, GroupModel group, int firstResult, int maxResults) {
+    	logger.info("SOAM User Storage: inside getGroupMembers");
         return Collections.EMPTY_LIST;
     }
 
     @Override
     public List<UserModel> getGroupMembers(RealmModel realm, GroupModel group) {
+    	logger.info("SOAM User Storage: inside getGroupMembers");
         return Collections.EMPTY_LIST;
     }
 
     @Override
     public List<UserModel> searchForUserByUserAttribute(String attrName, String attrValue, RealmModel realm) {
+    	logger.info("SOAM User Storage: inside searchForUserByUserAttribute");
         return Collections.EMPTY_LIST;
     }
 }
