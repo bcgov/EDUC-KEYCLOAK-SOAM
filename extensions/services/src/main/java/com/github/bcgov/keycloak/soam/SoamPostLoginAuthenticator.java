7bea6a95425412bad5727f936a2430aca00d4a1c
Marco Villeneuve <marco.villeneuve@gov.bc.ca>
2019-11-16 11:43:55 -0800

    Test.

--- extensions/services/src/main/java/com/github/bcgov/keycloak/soam/SoamPostLoginAuthenticator.java
+++ extensions/services/src/main/java/com/github/bcgov/keycloak/soam/SoamPostLoginAuthenticator.java
@@ -1,18 +1,27 @@
 package com.github.bcgov.keycloak.soam;
 
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
 import org.jboss.logging.Logger;
-import org.jboss.resteasy.spi.HttpRequest;
-import org.keycloak.KeycloakSecurityContext;
 import org.keycloak.authentication.AuthenticationFlowContext;
 import org.keycloak.authentication.authenticators.broker.AbstractIdpAuthenticator;
 import org.keycloak.authentication.authenticators.broker.util.SerializedBrokeredIdentityContext;
+import org.keycloak.broker.provider.AuthenticationRequest;
 import org.keycloak.broker.provider.BrokeredIdentityContext;
+import org.keycloak.broker.provider.IdentityProvider;
+import org.keycloak.broker.provider.IdentityProviderDataMarshaller;
+import org.keycloak.events.EventBuilder;
+import org.keycloak.models.FederatedIdentityModel;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
+import org.keycloak.models.UserSessionModel;
+import org.keycloak.representations.JsonWebToken;
+import org.keycloak.sessions.AuthenticationSessionModel;
 
 
-public class SoamPostLoginAuthenticator extends AbstractIdpAuthenticator {
+public class SoamPostLoginAuthenticator extends AbstractIdpAuthenticator implements IdentityProvider{
 
     private static Logger logger = Logger.getLogger(SoamPostLoginAuthenticator.class);
 
@@ -35,14 +44,7 @@ public class SoamPostLoginAuthenticator extends AbstractIdpAuthenticator {
         }
         
         
-        HttpRequest req = context.getHttpRequest();
-        
-        KeycloakSecurityContext kc = (KeycloakSecurityContext) req.getAttribute(KeycloakSecurityContext.class.getName());
-        if(kc == null) {
-        	logger.info("KSC is null");
-        }
-        logger.info("Token: " + kc.getTokenString());
-        
+       
         
 //        JsonWebToken token = (JsonWebToken)brokerContext.getContextData().get("VALIDATED_ID_TOKEN");
 //        
@@ -72,5 +74,64 @@ public class SoamPostLoginAuthenticator extends AbstractIdpAuthenticator {
     public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
         return true;
     }
+
+	@Override
+	public void preprocessFederatedIdentity(KeycloakSession session, RealmModel realm,
+			BrokeredIdentityContext context) {
+	
+	}
+
+	@Override
+	public void authenticationFinished(AuthenticationSessionModel authSession, BrokeredIdentityContext context) {
+		logger.info("SOAM Post: inside authenticationFinished");
+		logger.info("Token is: " + (JsonWebToken)context.getContextData().get("VALIDATED_ID_TOKEN"));
+		
+	}
+
+	@Override
+	public void importNewUser(KeycloakSession session, RealmModel realm, UserModel user,
+			BrokeredIdentityContext context) {
+	}
+
+	@Override
+	public void updateBrokeredUser(KeycloakSession session, RealmModel realm, UserModel user,
+			BrokeredIdentityContext context) {
+	}
+
+	@Override
+	public Object callback(RealmModel realm, AuthenticationCallback callback, EventBuilder event) {
+		return null;
+	}
+
+	@Override
+	public Response performLogin(AuthenticationRequest request) {
+		return null;
+	}
+
+	@Override
+	public Response retrieveToken(KeycloakSession session, FederatedIdentityModel identity) {
+		return null;
+	}
+
+	@Override
+	public void backchannelLogout(KeycloakSession session, UserSessionModel userSession, UriInfo uriInfo,
+			RealmModel realm) {
+	}
+
+	@Override
+	public Response keycloakInitiatedBrowserLogout(KeycloakSession session, UserSessionModel userSession,
+			UriInfo uriInfo, RealmModel realm) {
+		return null;
+	}
+
+	@Override
+	public Response export(UriInfo uriInfo, RealmModel realm, String format) {
+		return null;
+	}
+
+	@Override
+	public IdentityProviderDataMarshaller getMarshaller() {
+		return null;
+	}
 
 }
