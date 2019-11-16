039c37e486bbbbb8c4e12d8657e8ef37490096b0
Marco Villeneuve <marco.villeneuve@gov.bc.ca>
2019-11-16 09:24:01 -0800

    Test.

--- extensions/services/src/main/java/com/github/bcgov/keycloak/soam/SoamPostLoginAuthenticator.java
+++ extensions/services/src/main/java/com/github/bcgov/keycloak/soam/SoamPostLoginAuthenticator.java
@@ -2,12 +2,16 @@ package com.github.bcgov.keycloak.soam;
 
 import org.jboss.logging.Logger;
 import org.keycloak.authentication.AuthenticationFlowContext;
+import org.keycloak.authentication.AuthenticationFlowError;
+import org.keycloak.authentication.AuthenticationFlowException;
 import org.keycloak.authentication.authenticators.broker.AbstractIdpAuthenticator;
 import org.keycloak.authentication.authenticators.broker.util.SerializedBrokeredIdentityContext;
 import org.keycloak.broker.provider.BrokeredIdentityContext;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.RealmModel;
 import org.keycloak.models.UserModel;
+import org.keycloak.representations.JsonWebToken;
+import org.keycloak.sessions.AuthenticationSessionModel;
 
 
 public class SoamPostLoginAuthenticator extends AbstractIdpAuthenticator {
@@ -24,7 +28,7 @@ public class SoamPostLoginAuthenticator extends AbstractIdpAuthenticator {
     @Override
     public void authenticate(AuthenticationFlowContext context) {
     	logger.info("SOAM Post: inside authenticate");
-       
+        
         logger.info("context.getUser(): " + context.getUser());
         logger.info("context.getSession(): " + context.getSession());
         
@@ -32,7 +36,22 @@ public class SoamPostLoginAuthenticator extends AbstractIdpAuthenticator {
         	logger.info("User GUID: " + context.getUser().getFirstAttribute("GUID"));
         }
         
+        AuthenticationSessionModel authSession = context.getAuthenticationSession();
+        SerializedBrokeredIdentityContext serializedCtx = SerializedBrokeredIdentityContext.readFromAuthenticationSession(authSession, EXISTING_USER_INFO);
+        if (serializedCtx == null) {
+            throw new AuthenticationFlowException("Not found serialized context in clientSession", AuthenticationFlowError.IDENTITY_PROVIDER_ERROR);
+        }
+        BrokeredIdentityContext brokerContext = serializedCtx.deserialize(context.getSession(), authSession);
+
+        JsonWebToken token = (JsonWebToken)brokerContext.getContextData().get("VALIDATED_ID_TOKEN");
+        
+        for(String s: token.getOtherClaims().keySet()) {
+        	logger.info("Key: " + s + " Value: " + token.getOtherClaims().get(s));
+        }
+        
         UserModel existingUser = context.getSession().users().getUserByUsername(context.getUser().getUsername(), context.getRealm());
+        
+        
         context.setUser(existingUser);
         context.success();
     }
