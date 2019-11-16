7c56b195d95fcb9c783b0c6a56534e029e6dd644
Marco Villeneuve <marco.villeneuve@gov.bc.ca>
2019-11-15 16:33:06 -0800

    Test.

--- extensions/services/src/main/java/com/github/bcgov/keycloak/soam/SoamProtocolMapper.java
+++ extensions/services/src/main/java/com/github/bcgov/keycloak/soam/SoamProtocolMapper.java
@@ -68,6 +68,8 @@ public class SoamProtocolMapper extends AbstractOIDCProtocolMapper implements OI
     	logger.info("Protocol Mapper - User GUID is: " + userSession.getUser().getUsername());
     	token.getOtherClaims().put("given_name", "Working Test Claim");
     	token.getOtherClaims().put("preferred_username", "New Username");
+    	token.setPreferredUsername("ABCD");
+    	token.getOtherClaims().remove("preferred_username");
     }
 
     public static ProtocolMapperModel create(String name,
