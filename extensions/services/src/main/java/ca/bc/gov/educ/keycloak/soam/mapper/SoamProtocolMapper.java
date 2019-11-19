527177d1d3c988cb70cae326b4d1f527086d1ffd
Marco Villeneuve <marco.villeneuve@gov.bc.ca>
2019-11-19 14:50:35 -0800

    Updates.

--- extensions/services/src/main/java/ca/bc/gov/educ/keycloak/soam/mapper/SoamProtocolMapper.java
+++ extensions/services/src/main/java/ca/bc/gov/educ/keycloak/soam/mapper/SoamProtocolMapper.java
@@ -28,8 +28,6 @@ import org.keycloak.protocol.oidc.mappers.UserInfoTokenMapper;
 import org.keycloak.provider.ProviderConfigProperty;
 import org.keycloak.representations.IDToken;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
 import com.github.javafaker.Faker;
