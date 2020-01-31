6829d105174b8d59618db73f9b04cfb2cde762d6
Marco Villeneuve <marco.villeneuve@gov.bc.ca>
2020-01-31 12:50:42 -0800

    Small change to remove logging.

--- extensions/services/src/main/java/ca/bc/gov/educ/keycloak/soam/authenticator/SoamFirstTimeLoginAuthenticator.java
+++ extensions/services/src/main/java/ca/bc/gov/educ/keycloak/soam/authenticator/SoamFirstTimeLoginAuthenticator.java
@@ -45,8 +45,6 @@ public class SoamFirstTimeLoginAuthenticator extends AbstractIdpAuthenticator {
         
         JsonWebToken token = (JsonWebToken)brokerContext.getContextData().get("VALIDATED_ID_TOKEN");
         
-        logger.info("JWT token is: " + token);
-        
 		for(String s: token.getOtherClaims().keySet()) {
     		logger.info("Key: " + s + " Value: " + token.getOtherClaims().get(s));
 		}
