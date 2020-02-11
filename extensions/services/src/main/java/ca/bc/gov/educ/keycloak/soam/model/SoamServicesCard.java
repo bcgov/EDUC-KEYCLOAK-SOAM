5c511c3d6968898e15d679c9621772b5c0c2af75
Marco Villeneuve <marco.villeneuve@gov.bc.ca>
2020-02-11 10:20:34 -0800

    Testing BCSC.

---
+++ extensions/services/src/main/java/ca/bc/gov/educ/keycloak/soam/model/SoamServicesCard.java
@@ -0,0 +1,170 @@
+package ca.bc.gov.educ.keycloak.soam.model;
+
+import java.util.Date;
+import java.util.UUID;
+
+public class SoamServicesCard {
+	private UUID servicesCardInfoID;
+	private String did;
+	private String userDisplayName;
+	private String givenName;
+	private String givenNames;
+	private String surname;
+	private String birthDate;
+	private String gender;
+	private String email;
+	private String streetAddress;
+	private String city;
+	private String province;
+	private String country;
+	private String postalCode;
+	private String createUser;
+	private Date createDate;
+	private String updateUser;
+	private Date updateDate;
+
+	public UUID getServicesCardInfoID() {
+		return servicesCardInfoID;
+	}
+
+	public void setServicesCardInfoID(UUID servicesCardInfoID) {
+		this.servicesCardInfoID = servicesCardInfoID;
+	}
+
+	public String getDid() {
+		return did;
+	}
+
+	public void setDid(String did) {
+		this.did = did;
+	}
+
+	public String getUserDisplayName() {
+		return userDisplayName;
+	}
+
+	public void setUserDisplayName(String userDisplayName) {
+		this.userDisplayName = userDisplayName;
+	}
+
+	public String getGivenName() {
+		return givenName;
+	}
+
+	public void setGivenName(String givenName) {
+		this.givenName = givenName;
+	}
+
+	public String getGivenNames() {
+		return givenNames;
+	}
+
+	public void setGivenNames(String givenNames) {
+		this.givenNames = givenNames;
+	}
+
+	public String getSurname() {
+		return surname;
+	}
+
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
+
+	public String getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(String birthDate) {
+		this.birthDate = birthDate;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getStreetAddress() {
+		return streetAddress;
+	}
+
+	public void setStreetAddress(String streetAddress) {
+		this.streetAddress = streetAddress;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getProvince() {
+		return province;
+	}
+
+	public void setProvince(String province) {
+		this.province = province;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	public String getCreateUser() {
+		return createUser;
+	}
+
+	public void setCreateUser(String createUser) {
+		this.createUser = createUser;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getUpdateUser() {
+		return updateUser;
+	}
+
+	public void setUpdateUser(String updateUser) {
+		this.updateUser = updateUser;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+}
