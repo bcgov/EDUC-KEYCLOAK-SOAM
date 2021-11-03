c8ea428be4f1ff6db501ae579fdc7af6cfd7f861
arcshiftsolutions <arcshiftsolutions@gmail.com>
2021-11-02 17:03:18 -0700

    Fixes for SAML config.

--- extensions/services/src/main/java/ca/bc/gov/educ/keycloak/soam/authenticator/SoamFirstTimeSAMLLoginAuthenticator.java
+++ extensions/services/src/main/java/ca/bc/gov/educ/keycloak/soam/authenticator/SoamFirstTimeSAMLLoginAuthenticator.java
@@ -22,132 +22,136 @@ import java.util.Set;
 /**
  * SOAM First Time login authenticator
  * This class will handle the callouts to our API
- * 
- * @author Marco Villeneuve
  *
+ * @author Marco Villeneuve
  */
 public class SoamFirstTimeSAMLLoginAuthenticator extends AbstractIdpAuthenticator {
 
-    private static Logger logger = Logger.getLogger(SoamFirstTimeSAMLLoginAuthenticator.class);
+  private static Logger logger = Logger.getLogger(SoamFirstTimeSAMLLoginAuthenticator.class);
 
 
-    @Override
-    protected void actionImpl(AuthenticationFlowContext context, SerializedBrokeredIdentityContext serializedCtx, BrokeredIdentityContext brokerContext) {
-    } 
- 
-    @Override
-    protected void authenticateImpl(AuthenticationFlowContext context, SerializedBrokeredIdentityContext serializedCtx, BrokeredIdentityContext brokerContext) {
-		logger.debug("SOAM: inside SAML authenticateImpl");
-		KeycloakSession session = context.getSession();
-		RealmModel realm = context.getRealm();
+  @Override
+  protected void actionImpl(AuthenticationFlowContext context, SerializedBrokeredIdentityContext serializedCtx, BrokeredIdentityContext brokerContext) {
+  }
 
-		if (context.getAuthenticationSession().getAuthNote(EXISTING_USER_INFO) != null) {
-			context.attempted();
-			return;
-		}
+  @Override
+  protected void authenticateImpl(AuthenticationFlowContext context, SerializedBrokeredIdentityContext serializedCtx, BrokeredIdentityContext brokerContext) {
+    logger.debug("SOAM: inside SAML authenticateImpl");
+    KeycloakSession session = context.getSession();
+    RealmModel realm = context.getRealm();
 
-		Map<String, Object> brokerClaims = brokerContext.getContextData();
-		for(String s: brokerClaims.keySet()) {
-			logger.info("Context Key: " + s + " Value: " + brokerClaims.get(s));
-		}
+    if (context.getAuthenticationSession().getAuthNote(EXISTING_USER_INFO) != null) {
+      context.attempted();
+      return;
+    }
 
-		AssertionType assertion = (AssertionType)brokerContext.getContextData().get("SAML_ASSERTION");
+    Map<String, Object> brokerClaims = brokerContext.getContextData();
+    for (String s : brokerClaims.keySet()) {
+      logger.debug("Context Key: " + s + " Value: " + brokerClaims.get(s));
+    }
 
-		String username = null;
-		String userGUID = null;
-		String displayName = null;
+    AssertionType assertion = (AssertionType) brokerContext.getContextData().get("SAML_ASSERTION");
 
-		Set<AttributeStatementType> otherClaims = assertion.getAttributeStatements();
-		for(AttributeStatementType s: otherClaims) {
-			logger.info("SAML Assertion Claims:");
-			for(AttributeStatementType.ASTChoiceType type: s.getAttributes()) {
-				String name = type.getAttribute().getName();
-				logger.info("SAML_ASSERTION Key: " + name + " Value: " + type.getAttribute().getAttributeValue());
+    String username = null;
+    String userGUID = null;
+    String displayName = null;
+    String email = null;
 
-				if(name.equalsIgnoreCase("useridentifier")){
-					userGUID = (String)type.getAttribute().getAttributeValue().get(0);
-				}else if(name.equalsIgnoreCase("user_name")){
-					username = (String)type.getAttribute().getAttributeValue().get(0);
-				}else if(name.equalsIgnoreCase("SMGOV_USERDISPLAYNAME")){
-					displayName = (String)type.getAttribute().getAttributeValue().get(0);
-				}
-			}
-		}
+    Set<AttributeStatementType> otherClaims = assertion.getAttributeStatements();
+    for (AttributeStatementType s : otherClaims) {
+      logger.info("SAML Assertion Claims:");
+      for (AttributeStatementType.ASTChoiceType type : s.getAttributes()) {
+        String name = type.getAttribute().getName();
+        logger.info("SAML_ASSERTION Key: " + name + " Value: " + type.getAttribute().getAttributeValue());
 
-		String accountType = "bceid";
-
-		switch (accountType) {
-		case "bceid":
-			logger.debug("SOAM: Account type bceid found");
-			if(userGUID == null) {
-				throw new SoamRuntimeException("No bceid_guid value was found in token");
-			}
-			createOrUpdateUser(userGUID, accountType, "BASIC");
-			break;
-		case "idir":
-			logger.debug("SOAM: Account type idir found");
-			if(userGUID == null) {
-				throw new SoamRuntimeException("No idir_guid value was found in token");
-			}
-			break;
-		default:
-			throw new SoamRuntimeException("Account type is not bcsc, bceid or idir, check IDP mappers");
-		}
-
-        if(context.getSession().users().getUserByUsername(userGUID, realm) == null) {
-            logger.debugf("No duplication detected. Creating account for user '%s' and linking with identity provider '%s' .",
-					userGUID, brokerContext.getIdpConfig().getAlias());
-
-            UserModel federatedUser = session.users().addUser(realm, userGUID);
-            federatedUser.setEnabled(true);
-
-            if(accountType.equals("bceid")) {
-	           federatedUser.setSingleAttribute("display_name", displayName);
-	           federatedUser.setSingleAttribute("bceid_userid", userGUID);
-	           federatedUser.setLastName(username + "@bceid");
-            }else if(accountType.equals("idir")) {
- 	           federatedUser.setSingleAttribute("idir_username", userGUID);
- 	           federatedUser.setSingleAttribute("display_name", displayName);
-   	           //federatedUser.setFirstName(((String)otherClaims.get("given_name")));
-  	           //federatedUser.setLastName(((String)otherClaims.get("family_name")));
-            }
-
-            for (Map.Entry<String, List<String>> attr : serializedCtx.getAttributes().entrySet()) {
-                federatedUser.setAttribute(attr.getKey(), attr.getValue());
-            }
-
-            context.setUser(federatedUser);
-            context.getAuthenticationSession().setAuthNote(BROKER_REGISTERED_NEW_USER, "true");
-            context.success();
-        } else {
-        	logger.debug("SOAM: Existing " + accountType + " user found with username: " + userGUID);
-        	UserModel existingUser = context.getSession().users().getUserByUsername(userGUID, realm);
-        	context.setUser(existingUser);
-        	context.success();
+        if (name.equalsIgnoreCase("useridentifier")) {
+          userGUID = (String) type.getAttribute().getAttributeValue().get(0);
+        } else if (name.equalsIgnoreCase("user_name")) {
+          username = (String) type.getAttribute().getAttributeValue().get(0);
+        } else if (name.equalsIgnoreCase("SMGOV_USERDISPLAYNAME")) {
+          displayName = (String) type.getAttribute().getAttributeValue().get(0);
+        } else if (name.equalsIgnoreCase("Email")) {
+          email = (String) type.getAttribute().getAttributeValue().get(0);
         }
+      }
     }
 
-    protected void createOrUpdateUser(String guid, String accountType, String credType) {
-    	logger.debug("SOAM: createOrUpdateUser");
-    	logger.debug("SOAM: performing login for " + accountType + " user: " + guid);
-    	
-    	try {
-			RestUtils.getInstance().performLogin(credType, guid, guid, null);
-		} catch (Exception e) {
-			logger.error("Exception occurred within SOAM while processing login" + e.getMessage());
-			throw new SoamRuntimeException("Exception occurred within SOAM while processing login, check downstream logs for SOAM API service");
-		}
-    }
-    
-    
-    @Override
-    public boolean requiresUser() {
-        return false;
+    String accountType = (String) brokerClaims.get("user.attributes.account_type");
+
+    switch (accountType) {
+      case "bceid":
+        logger.debug("SOAM: Account type bceid found");
+        if (userGUID == null) {
+          throw new SoamRuntimeException("No bceid_guid value was found in token");
+        }
+        createOrUpdateUser(userGUID, accountType, "BASIC");
+        break;
+      case "idir":
+        logger.debug("SOAM: Account type idir found");
+        if (userGUID == null) {
+          throw new SoamRuntimeException("No idir_guid value was found in token");
+        }
+        break;
+      default:
+        throw new SoamRuntimeException("Account type is not bcsc, bceid or idir, check IDP mappers");
     }
 
-    @Override
-    public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
-        return true;
+    if (context.getSession().users().getUserByUsername(userGUID, realm) == null) {
+      logger.debugf("No duplication detected. Creating account for user '%s' and linking with identity provider '%s' .",
+        userGUID, brokerContext.getIdpConfig().getAlias());
+
+      UserModel federatedUser = session.users().addUser(realm, userGUID);
+      federatedUser.setEnabled(true);
+
+      if (accountType.equals("bceid")) {
+        federatedUser.setSingleAttribute("display_name", displayName);
+        federatedUser.setSingleAttribute("bceid_userid", userGUID);
+        federatedUser.setSingleAttribute("user_guid", userGUID);
+        federatedUser.setLastName(username + "@bceid");
+      } else if (accountType.equals("idir")) {
+        federatedUser.setSingleAttribute("idir_username", username);
+        federatedUser.setSingleAttribute("idir_guid", userGUID);
+        federatedUser.setSingleAttribute("user_guid", userGUID);
+        federatedUser.setSingleAttribute("display_name", displayName);
+        federatedUser.setSingleAttribute("email", email);
+      }
+
+      for (Map.Entry<String, List<String>> attr : serializedCtx.getAttributes().entrySet()) {
+        federatedUser.setAttribute(attr.getKey(), attr.getValue());
+      }
+
+      context.setUser(federatedUser);
+      context.getAuthenticationSession().setAuthNote(BROKER_REGISTERED_NEW_USER, "true");
+      context.success();
+    } else {
+      logger.debug("SOAM: Existing " + accountType + " user found with username: " + userGUID);
+      UserModel existingUser = context.getSession().users().getUserByUsername(userGUID, realm);
+      context.setUser(existingUser);
+      context.success();
     }
+  }
+
+  protected void createOrUpdateUser(String guid, String accountType, String credType) {
+    logger.debug("SOAM: createOrUpdateUser");
+    logger.debug("SOAM: performing login for " + accountType + " user: " + guid);
+
+    try {
+      RestUtils.getInstance().performLogin(credType, guid, guid, null);
+    } catch (Exception e) {
+      logger.error("Exception occurred within SOAM while processing login" + e.getMessage());
+      throw new SoamRuntimeException("Exception occurred within SOAM while processing login, check downstream logs for SOAM API service");
+    }
+  }
+
+
+  @Override
+  public boolean requiresUser() {
+    return false;
+  }
+
+  @Override
+  public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
+    return true;
+  }
 
 }
--- extensions/services/src/main/java/ca/bc/gov/educ/keycloak/soam/authenticator/SoamPostSAMLLoginAuthenticator.java
+++ extensions/services/src/main/java/ca/bc/gov/educ/keycloak/soam/authenticator/SoamPostSAMLLoginAuthenticator.java
@@ -26,115 +26,128 @@ import java.util.Set;
 
 public class SoamPostSAMLLoginAuthenticator extends AbstractIdpAuthenticator {
 
-    private static Logger logger = Logger.getLogger(SoamPostSAMLLoginAuthenticator.class);
+  private static Logger logger = Logger.getLogger(SoamPostSAMLLoginAuthenticator.class);
 
 
-    @Override
-    protected void actionImpl(AuthenticationFlowContext context, SerializedBrokeredIdentityContext serializedCtx, BrokeredIdentityContext brokerContext) {
-    }
+  @Override
+  protected void actionImpl(AuthenticationFlowContext context, SerializedBrokeredIdentityContext serializedCtx, BrokeredIdentityContext brokerContext) {
+  }
 
-    @Override
-    public void authenticate(AuthenticationFlowContext context) {
-        JsonReader reader = null;
-        try {
-            logger.debug("SOAM Post: inside authenticate");
+  @Override
+  public void authenticate(AuthenticationFlowContext context) {
+    JsonReader reader = null;
+    try {
+      logger.debug("SOAM Post: inside authenticate");
 
-            if (context.getAuthenticationSession().getAuthNote(BROKER_REGISTERED_NEW_USER) != null) {
-                context.setUser(context.getUser());
-                context.success();
-                return;
-            }
+      if (context.getAuthenticationSession().getAuthNote(BROKER_REGISTERED_NEW_USER) != null) {
+        context.setUser(context.getUser());
+        context.success();
+        return;
+      }
 
-            String stringSerialCtx = context.getAuthenticationSession().getAuthNote(PostBrokerLoginConstants.PBL_BROKERED_IDENTITY_CONTEXT);
-            SerializedBrokeredIdentityContext serializedCtx = JsonSerialization.readValue(stringSerialCtx, SerializedBrokeredIdentityContext.class);
-            BrokeredIdentityContext brokerContext = serializedCtx.deserialize(context.getSession(), context.getAuthenticationSession());
+      String stringSerialCtx = context.getAuthenticationSession().getAuthNote(PostBrokerLoginConstants.PBL_BROKERED_IDENTITY_CONTEXT);
+      SerializedBrokeredIdentityContext serializedCtx = JsonSerialization.readValue(stringSerialCtx, SerializedBrokeredIdentityContext.class);
+      BrokeredIdentityContext brokerContext = serializedCtx.deserialize(context.getSession(), context.getAuthenticationSession());
 
-            String accountType = context.getUser().getFirstAttribute("account_type");
+      String accountType = context.getUser().getFirstAttribute("account_type");
 
-            if (accountType == null) {
-                throw new SoamRuntimeException("Account type is null; account type should always be available, check the IDP mappers for the hardcoded attribute");
-            }
+      if (accountType == null) {
+        throw new SoamRuntimeException("Account type is null; account type should always be available, check the IDP mappers for the hardcoded attribute");
+      }
 
-            AssertionType assertion = (AssertionType)brokerContext.getContextData().get("SAML_ASSERTION");
+      AssertionType assertion = (AssertionType) brokerContext.getContextData().get("SAML_ASSERTION");
 
-            String userGUID = null;
-            String displayName = null;
+      String username = null;
+      String userGUID = null;
+      String displayName = null;
+      String email = null;
 
-            Set<AttributeStatementType> otherClaims = assertion.getAttributeStatements();
-            for(AttributeStatementType s: otherClaims) {
-                logger.info("SAML Assertion Claims:");
-                for(AttributeStatementType.ASTChoiceType type: s.getAttributes()) {
-                    String name = type.getAttribute().getName();
-                    logger.info("SAML_ASSERTION Key: " + name + " Value: " + type.getAttribute().getAttributeValue());
+      Set<AttributeStatementType> otherClaims = assertion.getAttributeStatements();
+      for (AttributeStatementType s : otherClaims) {
+        logger.info("SAML Assertion Claims:");
+        for (AttributeStatementType.ASTChoiceType type : s.getAttributes()) {
+          String name = type.getAttribute().getName();
+          logger.info("SAML_ASSERTION Key: " + name + " Value: " + type.getAttribute().getAttributeValue());
 
-                    if(name.equalsIgnoreCase("useridentifier")){
-                        userGUID = (String)type.getAttribute().getAttributeValue().get(0);
-                        break;
-                    }
-                }
-            }
-
-           // String username = null;
-
-            switch (accountType) {
-                case "bceid":
-                    logger.debug("SOAM Post: Account type bceid found");
-                    userGUID = context.getUser().getUsername();
-                    if (userGUID == null) {
-                        throw new SoamRuntimeException("No bceid_guid value was found in token");
-                    }
-                    updateUserInfo(userGUID, accountType, "BASIC", null);
-                    break;
-                case "idir":
-                    logger.debug("SOAM Post: Account type idir found");
-                    userGUID = context.getUser().getUsername();
-                    if (userGUID == null) {
-                        throw new SoamRuntimeException("No idir_guid value was found in token");
-                    }
-                    break;
-                default:
-                    throw new SoamRuntimeException("Account type is not bcsc, bceid or idir, check IDP mappers");
-            }
-
-            //UserModel existingUser = context.getSession().users().getUserByUsername(context.getUser().getUsername(), context.getRealm());
-
-            context.setUser(context.getUser());
-            context.success();
-        } catch (Exception e) {
-            throw new SoamRuntimeException(e);
-        } finally {
-            if (reader != null) {
-                reader.close();
-            }
+          if (name.equalsIgnoreCase("useridentifier")) {
+            userGUID = (String) type.getAttribute().getAttributeValue().get(0);
+          } else if (name.equalsIgnoreCase("user_name")) {
+            username = (String) type.getAttribute().getAttributeValue().get(0);
+          } else if (name.equalsIgnoreCase("SMGOV_USERDISPLAYNAME")) {
+            displayName = (String) type.getAttribute().getAttributeValue().get(0);
+          } else if (name.equalsIgnoreCase("Email")) {
+            email = (String) type.getAttribute().getAttributeValue().get(0);
+          }
         }
-    }
+      }
 
-    protected void updateUserInfo(String guid, String accountType, String credType, SoamServicesCard servicesCard) {
-        logger.debug("SOAM: createOrUpdateUser");
-        logger.debug("SOAM: performing login for " + accountType + " user: " + guid);
+      UserModel existingUser = context.getUser();
 
-        try {
-            RestUtils.getInstance().performLogin(credType, guid, guid, servicesCard);
-        } catch (Exception e) {
-            logger.error("Exception occurred within SOAM while processing login" + e.getMessage());
-            throw new SoamRuntimeException("Exception occurred within SOAM while processing login, check downstream logs for SOAM API service");
-        }
-    }
+      switch (accountType) {
+        case "bceid":
+          logger.debug("SOAM Post: Account type bceid found");
+          existingUser.setSingleAttribute("display_name", displayName);
+          existingUser.setSingleAttribute("bceid_userid", userGUID);
+          existingUser.setSingleAttribute("user_guid", userGUID);
 
-    @Override
-    protected void authenticateImpl(AuthenticationFlowContext context, SerializedBrokeredIdentityContext serializedCtx, BrokeredIdentityContext brokerContext) {
-        logger.debug("SOAM Post: inside returning authenticateImpl");
-        //Not used for Post Login Authenticator
-    }
+          if (userGUID == null) {
+            throw new SoamRuntimeException("No bceid_guid value was found in token");
+          }
+          updateUserInfo(userGUID, accountType, "BASIC", null);
+          break;
+        case "idir":
+          logger.debug("SOAM Post: Account type idir found");
+          existingUser.setSingleAttribute("idir_username", username);
+          existingUser.setSingleAttribute("idir_guid", userGUID);
+          existingUser.setSingleAttribute("user_guid", userGUID);
+          existingUser.setSingleAttribute("display_name", displayName);
+          existingUser.setSingleAttribute("email", email);
 
-    @Override
-    public boolean requiresUser() {
-        return false;
-    }
+          if (userGUID == null) {
+            throw new SoamRuntimeException("No idir_guid value was found in token");
+          }
+          break;
+        default:
+          throw new SoamRuntimeException("Account type is not bcsc, bceid or idir, check IDP mappers");
+      }
 
-    @Override
-    public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
-        return true;
+      context.setUser(context.getUser());
+      context.success();
+    } catch (Exception e) {
+      throw new SoamRuntimeException(e);
+    } finally {
+      if (reader != null) {
+        reader.close();
+      }
     }
+  }
+
+  protected void updateUserInfo(String guid, String accountType, String credType, SoamServicesCard servicesCard) {
+    logger.debug("SOAM: createOrUpdateUser");
+    logger.debug("SOAM: performing login for " + accountType + " user: " + guid);
+
+    try {
+      RestUtils.getInstance().performLogin(credType, guid, guid, servicesCard);
+    } catch (Exception e) {
+      logger.error("Exception occurred within SOAM while processing login" + e.getMessage());
+      throw new SoamRuntimeException("Exception occurred within SOAM while processing login, check downstream logs for SOAM API service");
+    }
+  }
+
+  @Override
+  protected void authenticateImpl(AuthenticationFlowContext context, SerializedBrokeredIdentityContext serializedCtx, BrokeredIdentityContext brokerContext) {
+    logger.debug("SOAM Post: inside returning authenticateImpl");
+    //Not used for Post Login Authenticator
+  }
+
+  @Override
+  public boolean requiresUser() {
+    return false;
+  }
+
+  @Override
+  public boolean configuredFor(KeycloakSession session, RealmModel realm, UserModel user) {
+    return true;
+  }
 
 }
